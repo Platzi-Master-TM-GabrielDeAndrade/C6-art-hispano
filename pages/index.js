2347da4cabadc69302956ffb6484ef993ae6be84
Elvix Marrugo <elvismarrugo@gmail.com>
2020-10-04 17:47:52 -0500

    environment variables

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
-export default function Home() {
+import { useState, useEffect } from 'react';
+import { firestore } from '../utils/firebase.config';
+
+const Index = () => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const query = await firestore.collection('users').get();
+    const users = query.docs.map((user) => {
+      return {
+        id: user.id,
+        ...user.data(),
+      };
+    });
+
+    setUsers(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div>
-      <h1>Art-Hispano</h1>
+      <p>Bienvenidos a Art-Hixpano</p>
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.email} {user.password}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
+
+export default Index;
---
+++ utils/firebase.config.js
@@ -0,0 +1,18 @@
+import * as Firebase from 'firebase/app';
+import 'firebase/firestore';
+
+console.log(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
+
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
+
+// Initialize Firebase
+const firebase = Firebase.apps.length
+  ? Firebase.app()
+  : Firebase.initializeApp(firebaseConfig);
+
+const firestore = Firebase.firestore();
+
+module.exports = {
+  firebase,
+  firestore,
+};
