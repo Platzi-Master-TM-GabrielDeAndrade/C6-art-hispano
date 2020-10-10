d337ebda477818c6be72b0565d60151c8d49b2b6
MrElvix <elvismarrugo@gmail.com>
2020-10-10 07:23:38 -0500

    fix error sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-// // import "../styles/globals.scss";
-// import "../styles/styles.scss";
-// // import "../styles/styles.css";
-import "../styles.scss";
+import "../styles/styles.scss";
+import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
