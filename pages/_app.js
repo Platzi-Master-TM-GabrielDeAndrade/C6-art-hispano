428a22ea3f63746c8a4280e78bd98322045fd5c1
MrElvix <elvismarrugo@gmail.com>
2020-10-10 08:15:48 -0500

    fix pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/styles.scss";
 import "../styles/globals.scss";
+import "../styles/components/Navbar.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
