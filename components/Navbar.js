691a78a78e9df9e3ba334111d4f452f126a90319
MrElvix <elvismarrugo@gmail.com>
2020-10-26 14:35:38 -0500

    link to login

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,33 +18,43 @@ export default function Navbar() {
     }, []);
   return firebaseUser !== false ? (
     <header>
-      <nav className = {styles.nav_container}>
-
-        <div className = {styles.nav_containerLogo}>
+      <nav className={styles.nav_container}>
+        <div className={styles.nav_containerLogo}>
           <Link href="/">
-          <img src="logo.svg" alt="logo" className = {styles.nav_logo}/>
-          </Link>     
+            <img src="logo.svg" alt="logo" className={styles.nav_logo} />
+          </Link>
         </div>
 
-        <div className = {styles.nav_inputs}>  
+        <div className={styles.nav_inputs}>
           <input type="text" />
-          <button><img src="search.svg" alt="" width="15" height="15" /></button>
+          <button>
+            <img src="search.svg" alt="" width="15" height="15" />
+          </button>
           <p>Categotira</p>
           <p>Ofertas de la semana</p>
           <p>Filtrar</p>
         </div>
-        
-       <div className = {styles.nav_icons}>
-          <button type = "text" value="hola" > Vender</button>
-          <img src="carrito.svg" alt="Carrito" width="30" height="30"/>
-          <img src="favorito.svg" alt="Favorito" width="30" height="30"/>
-          <img src="notificaciones.svg" alt="Notificaciones" width="30" height="30"/>
-          <img src="user.svg" alt="user" width="70" height="80"/>
+
+        <div className={styles.nav_icons}>
+          <button type="text" value="hola">
+            {" "}
+            Vender
+          </button>
+          <img src="carrito.svg" alt="Carrito" width="30" height="30" />
+          <img src="favorito.svg" alt="Favorito" width="30" height="30" />
+          <img
+            src="notificaciones.svg"
+            alt="Notificaciones"
+            width="30"
+            height="30"
+          />
+          <Link href="/login">
+            <img src="user.svg" alt="user" width="70" height="80" />
+          </Link>
         </div>
-            
-        </nav>
-      </header>
-     ) : (
+      </nav>
+    </header>
+  ) : (
     <p>Loading ..</p>
   );
 }
