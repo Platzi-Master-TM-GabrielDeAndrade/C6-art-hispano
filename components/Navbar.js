637c13d03c934efbf1457b0648c9f533b484d713
MrElvix <elvismarrugo@gmail.com>
2020-10-26 14:29:29 -0500

    fix navbar again

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,22 @@
 import Link from "next/link";
+import { auth } from "../firebase/firebase.config";
+import { useEffect, useState } from "react";
 import styles from "../styles/components/Navbar.module.scss";
 
 export default function Navbar() {
-  return (
+  const [firebaseUser, setFirebaseUser] = useState(false);
+
+    useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        console.log(user);
+        if (user) {
+          setFirebaseUser(user);
+        } else {
+          setFirebaseUser(null);
+        }
+      });
+    }, []);
+  return firebaseUser !== false ? (
     <header>
       <nav className = {styles.nav_container}>
 
@@ -30,66 +44,7 @@ export default function Navbar() {
             
         </nav>
       </header>
-    );
-  }
-import { auth } from "../firebase/firebase.config";
-import { useEffect, useState } from "react";
-
-const Navbar = () => {
-  
-  const [firebaseUser, setFirebaseUser] = useState(false);
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      console.log(user);
-      if (user) {
-        setFirebaseUser(user);
-      } else {
-        setFirebaseUser(null);
-      }
-    });
-  }, []);
-
-  return firebaseUser !== false ? (
-    <div>
-      <nav>
-        <menu>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-
-          <Link href="/admin">
-            <a>Admin</a>
-          </Link>
-
-          <Link href="/signup">
-            <a>Registrar</a>
-          </Link>
-
-          <Link href="/login">
-            <a>Ingresar</a>
-          </Link>
-
-          {/* <Link href="/about">
-            <a>About</a>
-          </Link> */}
-
-          <Link href="/users">
-            <a>Users</a>
-          </Link>
-
-          {/* <Link href="/login2">
-            <a>Login2</a>
-          </Link> */}
-          <Link href="/categorias">
-            <a>Categorias</a>
-          </Link>
-        </menu>
-      </nav>
-    </div>
-  ) : (
+     ) : (
     <p>Loading ..</p>
   );
 }
-
-export default Navbar;
