839aa4d41a4fc45485a6942316ae5d3093fddc5a
MrElvix <elvismarrugo@gmail.com>
2020-10-28 18:40:23 -0500

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,30 @@
-import styles from "@styles/components/Footer.module.scss";
-
+import styles from "../styles/components/Footer.module.scss";
 const Footer = () => (
   <footer className={styles.Footer}>
-    {/* <div className="container">
-      <span>&#10032; OneLess 2020</span>
-    </div> */}
-    <h3>Oportunidades</h3>
-    <p>Trabaja con nosotros Informacion Corportiva</p>
-    <h3>Haz Negocio</h3>
-    <p>Vende en Art-Hispano </p>
-    <h3>Informacion</h3>
-    <p>Contacto: 0000000</p>
-    <p>Carrera 12 # 45 -5333</p>
-    <p>www.art-hispano.com</p>
-    <h3>Ayuda</h3>    
+    
+      <div className={styles.AboutUsOportunidades}>
+        <h3>Oportunidades</h3>
+        <p>Trabaja con nosotros Informacion Corportiva</p>
+      </div>
+      <div className={styles.AboutUsHaz}>
+        <h3>Haz Negocio</h3>
+        <p>Vende en Art-Hispano </p>
+      </div>
+      <div className={styles.AboutUsInformacion}>
+        <h3>Informacion</h3>
+        <p>Contacto: 0000000</p>
+        <p>Carrera 12 # 45 -5333</p>
+        <p>www.art-hispano.com</p>
+      </div>
+      <div>
+        <h3 className={styles.AboutUsAyuda}>Ayuda</h3>
+      </div>
+    
+    <div className={styles.Social}>
+      <img src="instagram.svg" alt="logo" />
+      <img src="facebook.svg" alt="logo" />
+      <img src="twitter.svg" alt="logo" />
+    </div>
   </footer>
 );
 
