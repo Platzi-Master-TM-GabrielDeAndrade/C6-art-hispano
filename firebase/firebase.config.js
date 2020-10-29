71cbd4cdc7f1d309f07fb325ba1d940b832fa069
MrElvix <elvismarrugo@gmail.com>
2020-10-28 19:29:01 -0500

    fix error sell

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -2,7 +2,6 @@ import * as Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-
 // console.log(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
--- pages/sell.js
+++ pages/sell.js
@@ -1,6 +1,6 @@
 /* import styles from "../styles/pages/Index.module.scss"; */
 import styles from "@styles/pages/Sell.module.scss";
-import Button from "../components/Button";
+// import Button from "../components/Button";
 
 export default function sell () {
   return (
@@ -27,49 +27,49 @@ export default function sell () {
          </button>
       </div>
       <div className={styles.search_one}>
-         <select name="sell" class="sell-select">
+         <select name="sell" className="sell-select">
             <option value="/">Hogar</option>
             <option value="">Cocina</option>
             <option value="">Lenceria</option>
             <option value="">Decoración</option>
             <option value="">Accesorio para baño</option>
          </select>
-         <select name="sell" class="sell-select">
+         <select name="sell" className="sell-select">
             <option value="/">Bisuteria</option>
             <option value="">Collares</option>
             <option value="">Pulceras</option>
             <option value="">Manillas</option>
          </select>
-         <select name="sell" class="sell-select">
+         <select name="sell" className="sell-select">
             <option value="/">Instrumentos Musicales</option>
             <option value="">Guitarras</option>
             <option value="">Bongos</option>
             <option value="">Panderetas</option>
             <option value="">Flautas</option>
          </select>
-         <select name="sell" class="sell-select">
+         <select name="sell" className="sell-select">
             <option value="/">Ropa</option>
             <option value="">Blusas</option>
             <option value="">Sacos</option>
             <option value="">Pantalones</option>
             <option value="">Bufandas</option>
          </select>
-         <select name="sell" class="sell-select">
+         <select name="sell" className="sell-select">
             <option value="/">Calzado</option>
          </select>
       </div>
       <div className={styles.search_two}>
-       <select name="sell" class="sell-select">
+       <select name="sell" className="sell-select">
             <option value="/">Decoración</option>
             <option value="">Ceramica</option>
             <option value="">Madera</option>
             <option value="">Metal</option>
             <option value="">Otros</option>
          </select>
-         <select name="sell" class="sell-select">
+         <select name="sell" className="sell-select">
             <option value="/">Bricolaje</option>
          </select>
-         <select name="" class="sell-select">
+         <select name="" className="sell-select">
             <option value="/">Jardines</option>
          </select> 
        </div>
