9ba64c6692770e12e1ffef5de4a859d7dbd5b30a
KevinJZea <kevinjzea@gmail.com>
2020-11-01 21:39:01 -0600

    KZ. Shipping Page is done. Modified Button component and ProductShipping component. Created Shipping Options component and its syles.

---
+++ components/ShippingOptions.js
@@ -0,0 +1,36 @@
+import styles from "../styles/components/ShippingOptions.module.scss";
+import Button from "components/Button";
+
+
+const ShippingOptions = ( { address, customerName } ) => (    
+
+    <div>
+        <section className={styles.MainContainer}>
+            <section className={styles.AddressContainer}>
+                <h2>Opciones de Env&iacute;o</h2>
+                <p className={styles.Address}>Domicilio</p>
+                <p className={styles.AddressDescription}>Enviar a: {customerName}. {address}.</p>
+                <Button children="Cambiar direcci&oacute;n" style="RemoveShipping" />
+            </section>
+            <section className={styles.ShippingSpeed}>
+                <h3>Elegir velocidad de env&iacute;o</h3>
+                <p>¡Entrega garantizada!</p>
+                <div className={styles.ButtonContainer}>
+                    <Button style="ShippingBall" />
+                    <span>Env&iacute;o gratis</span>
+                </div>
+                <div className={styles.ButtonContainer}>
+                    <Button style="ShippingBall" />
+                    <span>2 d&iacute;as</span>
+                </div>
+                <div className={styles.ButtonContainer}>
+                    <Button style="ShippingBall" />
+                    <span>1 semana</span>
+                </div>
+            </section>
+        </section>
+            
+    </div>
+);
+
+export default ShippingOptions;
--- pages/shipping.js
+++ pages/shipping.js
@@ -2,6 +2,8 @@
 import styles from "../styles/pages/Shipping.module.scss";
 import ProductShipping from "components/ProductShipping";
 import ShippingSummary from "components/ShippingSummary";
+import Button from "@components/Button";
+import ShippingOptions from "components/ShippingOptions";
 
 
 export default function Shipping () {
@@ -10,13 +12,18 @@ export default function Shipping () {
         
         <main className={styles.Main}>
             <section className={styles.Container}>
+
+                <ShippingOptions customerName="Kevin J. Zea" address="Jardines de Galicia 7243, Col. Jardines de Babilonia, C. P. 68420, Abasolo, Nuevo León, México" />
+
                 <ProductShipping title="Cráneo Decorativo Azteca Dorado Artesanía Mexicana" description="Reliquia de los antiguos aztecas, encontrada en las ruinas de Tenochtitlán durante investigaciones arqueológicas en 1937. Cráneo bañado en oro y conservado por los museos de historia prehispánica en la Ciudad de México." price={19999.97} imgUrl="https://http2.mlstatic.com/crazdo-craneo-decorativo-azteca-dorado-artesania-mexicana-D_NQ_NP_954974-MLM31546603590_072019-F.jpg" quantity={3} />
+                <div className={styles.ButtonContainer}>
+                    <Button children="Confirmar Compra" style="Continue" />
+                </div>
             </section>
             <section className={styles.RightBar}>
                 <section className={styles.SummaryContainer}>
                     <ShippingSummary totalQuantity={2} totalPrice={19999.97*2} shippingCost={5000} totalCost={19999.97*2+5000} />
                 </section>
-                
             </section>
         </main>
 
