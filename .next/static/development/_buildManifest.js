6bed09887c270c53a68c8d326e5197fba69a4725
javitecsis <69080742+javitecsis@users.noreply.github.com>
2020-11-13 09:37:18 -0500

    resolucion conflicto

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/[product]":["static\u002Fchunks\u002Fpages\u002F[product].js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002F[product]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 80e23605ad4f6c79b2ab25563a2e51444fe85857
